fa255d2611f380c05dd2c301a0fad8372813c089
Andrii Badekha <badekha@gmail.com>
2021-08-08 21:04:56 +0300

    Part 2. Cards layout.

--- src/components/RestourantsListPage/RestourantsListPage.js
+++ src/components/RestourantsListPage/RestourantsListPage.js
@@ -1,6 +1,10 @@
 // import { directive } from "@babel/types";
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { RestaurantCard } from '../RestaurantCard';
+import './RestaurantsListPage.scss';
+
+const DEFAULT_ETA_RANGE = '20 - 30 min';
 
 export class RestourantsListPage extends Component {
   componentDidMount() {
@@ -13,10 +17,27 @@ export class RestourantsListPage extends Component {
     const { restaurantsData } = this.props;
 
     return (
-      <div>
-        <pre>
-          {JSON.stringify(restaurantsData, null, 4)}
-        </pre>
+      <div className="restaurants-list">
+        {restaurantsData.map((restaurant) => {
+          const {
+            uuid,
+            title,
+            heroImageUrl,
+            categories,
+            etaRange,
+          } = restaurant;
+
+          return (
+            <RestaurantCard
+              key={uuid}
+              uuid={uuid}
+              title={title}
+              imageUrl={heroImageUrl}
+              categories={categories}
+              etaRange={etaRange ? etaRange.text : DEFAULT_ETA_RANGE}
+            />
+          );
+        })}
       </div>
     );
   }
@@ -24,5 +45,9 @@ export class RestourantsListPage extends Component {
 
 RestourantsListPage.propTypes = {
   loadRestaurants: PropTypes.func.isRequired,
-  restaurantsData: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  restaurantsData: PropTypes.arrayOf(PropTypes.object.isRequired),
+};
+
+RestourantsListPage.defaultProps = {
+  restaurantsData: [],
 };
